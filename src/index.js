c6550e8f44e9b59689895b21052c044ffbcdd626
Arnaud Barré <arnaud.barre@kametventures.com>
2018-12-12 20:54:56 +0100

    Remove login and select. black and white favicon

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import axios from 'axios';
 
 axios.interceptors.response.use(res => res, console.error); // TODO: display error
 
-const options = categories.map(c => c.category).filter(c => c.startsWith('en'));
-
 const App = () => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
   const [result, setResult] = useState();
-  const [loading, setLoading] = useState(false);
-  const [option, setOption] = useState('');
-  const [focused, setFocused] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const next = () => {
     setLoading(true);
@@ -26,19 +20,12 @@ const App = () => {
       .finally(() => setLoading(false));
   };
 
-  const useAppCredentials = () => {
-    setLogin('hungergames');
-    setPassword('happyhungergames');
-    next();
-  };
-
   const edit = annotation => {
     setLoading(true);
-    setOption('');
     axios.post('http://robotoff.bournhonesque.eu/api/v1/categories/annotate',
       new URLSearchParams(`task_id=${result.task_id}&annotation=${annotation}&save=1`)
-    ).then(() => {
-      // TODO: display ok
+    ).then(({ data }) => {
+      console.log(data); // TODO: display ok
       next();
     });
   };
@@ -46,7 +33,7 @@ const App = () => {
   useEffect(
     () => {
       const keyDownHandle = event => {
-        if (result && !loading && !focused) {
+        if (result && !loading) {
           if (event.which === 75) edit(-1); // k
           if (event.which === 78) edit(0); // n
           if (event.which === 79) edit(1); // o
@@ -57,48 +44,17 @@ const App = () => {
         window.document.removeEventListener('keydown', keyDownHandle);
       };
     },
-    [loading, result, focused],
+    [loading, result],
   );
 
+  useEffect(() => next(), []);
+
   if (!result) {
-    return (
-      <div className="mt-5 d-flex align-items-center justify-content-center">
-        <div style={{ width: '300px' }} className="text-center">
-          <h4>Please enter your credentials to enable edits</h4>
-          <input
-            className="mt-2 form-control"
-            value={login}
-            placeholder="Login"
-            onChange={e => setLogin(e.target.value)}
-          />
-          <input
-            className="my-2 form-control"
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <button
-            disabled={!login || !password || loading}
-            className="btn btn-success btn-block"
-            onClick={next}
-          >
-            Start
-          </button>
-          <button
-            className="btn btn-secondary btn-block"
-            disabled={loading}
-            onClick={useAppCredentials}
-          >
-            Use app credentials
-          </button>
-        </div>
-      </div>
-    );
+    return <h4 className="mt-3 text-center">Loading...</h4>
   }
 
   return (
-    <div className="d-flex pt-3 flex-column text-center align-items-center">
+    <div className="d-flex mt-3 flex-column text-center align-items-center">
       <h4 style={{minHeight: '3.6rem'}} className="mb-0">{result.product.product_name}</h4>
       <h5>
         (
@@ -148,40 +104,6 @@ const App = () => {
         >
           Yes (o)
         </button>
-      </div>
-      <div className="d-flex flex-column mt-2" style={{ width: '300px' }}>
-        <input
-          placeholder="Search a category"
-          className="form-control"
-          value={option}
-          onChange={e => setOption(e.target.value)}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-        />
-        {options.includes(option) ? (
-          <button
-            disabled
-            className="btn btn-primary mt-1"
-          >
-            Select (available soon)
-          </button>
-        ) : (
-          <div className="list-group">
-            {option.length > 1 &&
-              options
-                .filter(o => o.includes(option))
-                .slice(0, 5)
-                .map(o => (
-                  <span
-                    key={o}
-                    className="list-group-item list-group-item-action"
-                    onClick={() => setOption(o)}
-                  >
-                    {o}
-                  </span>
-                ))}
-          </div>
-        )}
       </div>
     </div>
   );
