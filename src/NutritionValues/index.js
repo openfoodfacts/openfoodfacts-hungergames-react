ee66b8c4eec3d2f1778c1f6b49aab30a5371f883
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-08-25 22:46:55 +0200

    use the correct url for images

--- src/NutritionValues/index.js
+++ src/NutritionValues/index.js
@@ -1,22 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const getIngredientImageUrlFromCode = (code, language, rev, size) => {
-  console.log({ code, language, rev, size });
-  let url = 'https://static.openfoodfacts.org/images/products/';
-  url =
-    url +
-    code.substring(0, 3) +
-    '/' +
-    code.substring(3, 6) +
-    '/' +
-    code.substring(6, 9) +
-    '/' +
-    code.substring(9, 13);
-  url = url + '/nutrition_' + language + '.' + rev + '.' + size + '.jpg';
-  return url;
-};
-
 const useNumberOfPages = () => {
   const [nbOfPages, setNbOfPages] = useState();
   useEffect(() => {
@@ -41,7 +25,6 @@ const useGetProduct = nbOfPages => {
   const [productsBacklog, setProductsBacklog] = useState([]);
 
   useEffect(() => {
-    console.log(nbOfPages);
     if (nbOfPages && productsBacklog.length < 6) {
       const AddProducts = async () => {
         const randomPage = Math.floor(Math.random() * nbOfPages);
@@ -50,26 +33,15 @@ const useGetProduct = nbOfPages => {
         } = await axios(
           `${
             process.env.REACT_APP_OFF_BASE
-          }/state/photos-validated/state/nutrition-facts-to-be-completed/${randomPage}.json?fields=code,images,lang`,
+          }/state/photos-validated/state/nutrition-facts-to-be-completed/${randomPage}.json?fields=code,lang,image_nutrition_url`,
         );
-        products = products
-          .filter(product => {
-            console.log(product);
-            return Object.keys(product.images).includes(
-              `nutrition_${product.lang}`,
-            );
-          })
-          .map(product => {
-            console.log({
-              rev: product.images[`nutrition_${product.lang}`].rev,
-            });
-            return {
-              code: product.code,
-              rev: product.images[`nutrition_${product.lang}`].rev,
-              lang: product.lang,
-            };
-          });
-        console.log('end : ', [...productsBacklog, ...products]);
+        products = products.map(product => {
+          return {
+            code: product.code,
+            imageUrl: product.image_nutrition_url,
+            lang: product.lang,
+          };
+        });
         setProductsBacklog([...productsBacklog, ...products]);
       };
       AddProducts();
@@ -85,15 +57,7 @@ const NutritionValues = () => {
 
   return products && products.length > 0 ? (
     <>
-      <img
-        src={`${getIngredientImageUrlFromCode(
-          products[0].code,
-          products[0].lang,
-          products[0].rev,
-          '400',
-        )}`}
-        alt="product"
-      />
+      <img src={products[0].imageUrl} alt="product" />
       <button
         onClick={() => {
           setProducts(products.slice(1));
