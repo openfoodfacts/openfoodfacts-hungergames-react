f889b29726e8af15daf55f48319887a4f781442c
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-11-07 17:43:57 +0100

    correct typo

--- src/NutritionValues/nutriments.js
+++ src/NutritionValues/nutriments.js
@@ -1,7 +1,7 @@
 export default {
   Energy: 'nutriment_energy',
   Fat: 'nutriment_fat',
-  'Sat FAt': 'nutriment_saturated-fat',
+  'Sat Fat': 'nutriment_saturated-fat',
   Carbs: 'nutriment_carbohydrates',
   Sugar: 'nutriment_sugars',
   Proteins: 'nutriment_proteins',
